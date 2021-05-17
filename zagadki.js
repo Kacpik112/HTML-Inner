54212cea0cf59c678135cb9ead32d9751514c472
Kacpik112 <66215363+Kacpik112@users.noreply.github.com>
2021-05-17 08:10:42 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,55 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Ma Twoje imię, bo Ty prowadzisz autobus.";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="7 - A L F A B E T";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Trzy";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="Piłkę tenisową";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Czy już śpisz?";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
